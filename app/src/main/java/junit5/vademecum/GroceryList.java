80b3848c6fe75b794b074d8ca4ffbe984b685b79
aegroto <aegroto@protonmail.com>
2022-09-07 13:53:52 +0200

    simple tests

--- app/src/main/java/junit5/vademecum/GroceryList.java
+++ app/src/main/java/junit5/vademecum/GroceryList.java
@@ -1,5 +1,27 @@
 package junit5.vademecum;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GroceryList {
-    
+    private Set<String> items;
+
+    public GroceryList() {
+        this.items = new HashSet<>();
+    }
+
+    public void addItem(String newItem) {
+        boolean notPresent = this.items.add(newItem);
+        if(!notPresent) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public int itemsCount() {
+        return this.items.size();
+    }
+
+    public void clear() {
+        this.items.clear();
+    }
 }
--- app/src/test/java/junit5/vademecum/GroceryListTest.java
+++ app/src/test/java/junit5/vademecum/GroceryListTest.java
@@ -1,4 +1,30 @@
 package junit5.vademecum;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class GroceryListTest {
+    GroceryList groceryList;
+
+    @Test
+    void testItemsCount_whenOneItemIsAdded() {
+        // Arrange
+        groceryList = new GroceryList();
+        // Act
+        groceryList.addItem("Milk");
+        // Assert
+        assertEquals(1, groceryList.itemsCount());
+    }
+
+    @Test
+    void testItemsCount_whenTwoItemsAreAdded() {
+        // Arrange
+        groceryList = new GroceryList();
+        // Act
+        groceryList.addItem("Milk");
+        groceryList.addItem("Bread");
+        // Assert
+        assertEquals(2, groceryList.itemsCount());
+    }
 }
